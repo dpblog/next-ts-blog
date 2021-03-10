d63cbea28807d88839e17b8545d07c1f699d0a67
charoenwai <thaksin.sut@gmail.com>
2021-03-10 16:46:55 +0700

    [:tada:] create 404 keyframes

---
+++ styles/404.keyframes.tsx
@@ -0,0 +1,117 @@
+import styled, { keyframes } from "styled-components";
+
+export const colordancing1 = keyframes`
+  0% {
+    color: #4c2d3a;
+  }
+  25% {
+    color: #c8393e;
+  }
+  50% {
+    color: #f38d44;
+  }
+  75% {
+    color: #f0bf3e;
+  }
+  100% {
+    color: #4c2d3a;
+  }
+`;
+export const colordancing2 = keyframes`
+  0% {
+    color: #f0bf3e;
+  }
+  25% {
+    color: #4c2d3a;
+  }
+  50% {
+    color: #c8393e;
+  }
+  75% {
+    color: #f38d44;
+  }
+  100% {
+    color: #f0bf3e;
+  }
+`;
+export const colordancing3 = keyframes`
+  0% {
+    color: #f38d44;
+  }
+  25% {
+    color: #f0bf3e;
+  }
+  50% {
+    color: #4c2d3a;
+  }
+  75% {
+    color: #c8393e;
+  }
+  100% {
+    color: #f38d44;
+  }
+`;
+export const shadowsdancing1 = keyframes`
+  0% {
+    box-shadow: inset 30px 0 0 rgba(76, 45, 58, 0.4),
+				inset 0 30px 0 rgba(200, 57, 62, 0.4),
+				inset -30px 0 0 rgba(243, 141, 68, 0.4),	
+				inset 0 -30px 0 rgba(240, 190, 62, 0.4);
+  }
+  25% {
+    box-shadow: inset 30px 0 0 rgba(200, 57, 62, 0.4),
+				inset 0 30px 0 rgba(243, 141, 68, 0.4),
+				inset -30px 0 0 rgba(240, 190, 62, 0.4),	
+				inset 0 -30px 0 rgba(76, 45, 58, 0.4);
+  }
+  50% {
+     box-shadow: inset 30px 0 0 rgba(243, 141, 68, 0.4),
+				inset 0 30px 0 rgba(240, 190, 62, 0.4),
+				inset -30px 0 0 rgba(76, 45, 58, 0.4),	
+				inset 0 -30px 0 rgba(200, 57, 62, 0.4);
+  }
+  75% {
+   box-shadow: inset 30px 0 0 rgba(240, 190, 62, 0.4),
+				inset 0 30px 0 rgba(76, 45, 58, 0.4),
+				inset -30px 0 0 rgba(200, 57, 62, 0.4),	
+				inset 0 -30px 0 rgba(243, 141, 68, 0.4);
+  }
+  100% {
+    box-shadow: inset 30px 0 0 rgba(76, 45, 58, 0.4),
+				inset 0 30px 0 rgba(200, 57, 62, 0.4),
+				inset -30px 0 0 rgba(243, 141, 68, 0.4),	
+				inset 0 -30px 0 rgba(240, 190, 62, 0.4);
+  }
+`;
+export const shadowsdancing2 = keyframes`
+  0% {
+    box-shadow: inset 15px 0 0 rgba(76, 45, 58, 0.4),
+				inset 0 15px 0 rgba(200, 57, 62, 0.4),
+				inset -15px 0 0 rgba(243, 141, 68, 0.4),	
+				inset 0 -15px 0 rgba(240, 190, 62, 0.4);
+  }
+  25% {
+    box-shadow: inset 15px 0 0 rgba(200, 57, 62, 0.4),
+				inset 0 15px 0 rgba(243, 141, 68, 0.4),
+				inset -15px 0 0 rgba(240, 190, 62, 0.4),	
+				inset 0 -15px 0 rgba(76, 45, 58, 0.4);
+  }
+  50% {
+     box-shadow: inset 15px 0 0 rgba(243, 141, 68, 0.4),
+				inset 0 15px 0 rgba(240, 190, 62, 0.4),
+				inset -15px 0 0 rgba(76, 45, 58, 0.4),	
+				inset 0 -15px 0 rgba(200, 57, 62, 0.4);
+  }
+  75% {
+   box-shadow: inset 15px 0 0 rgba(240, 190, 62, 0.4),
+				inset 0 15px 0 rgba(76, 45, 58, 0.4),
+				inset -15px 0 0 rgba(200, 57, 62, 0.4),	
+				inset 0 -15px 0 rgba(243, 141, 68, 0.4);
+  }
+  100% {
+    box-shadow: inset 15px 0 0 rgba(76, 45, 58, 0.4),
+				inset 0 15px 0 rgba(200, 57, 62, 0.4),
+				inset -15px 0 0 rgba(243, 141, 68, 0.4),	
+				inset 0 -15px 0 rgba(240, 190, 62, 0.4);
+  }
+`;
